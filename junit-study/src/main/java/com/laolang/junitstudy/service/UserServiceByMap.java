6bc133c47f275ee7aa525cc97b785275043c0729
laolang2016 <xiaodaima2016@163.com>
2015-09-19 14:00:48 +0800

    完成： 	1、cobertura的使用

--- junit-study/src/main/java/com/laolang/junitstudy/service/UserServiceByMap.java
+++ junit-study/src/main/java/com/laolang/junitstudy/service/UserServiceByMap.java
@@ -6,20 +6,20 @@ import java.util.Map;
 import com.laolang.junitstudy.model.User;
 import com.laolang.junitstudy.model.UserException;
 
-public class UserServiceByMap implements IUserService{
-	
-	
+public class UserServiceByMap implements IUserService {
 
 	public void add(User user) {
-		if( null != load(user.getUsername())){
+		if (null != load(user.getUsername())) {
 			throw new UserException("用户已存在");
 		}
-		
+
 		us.put(user.getUsername(), user);
 	}
 
-
 	public void delete(String username) {
+		if ("root".equals(username)) {
+			return;
+		}
 		us.remove(username);
 	}
 
@@ -29,17 +29,15 @@ public class UserServiceByMap implements IUserService{
 
 	public User login(String username, String password) {
 		User user = load(username);
-		if( null == user ){
+		if (null == user) {
 			throw new UserException("用户名不存在");
 		}
-		
-		if( !password.equals(user.getPassword())){
+
+		if (!password.equals(user.getPassword())) {
 			throw new UserException("用户密码错误");
 		}
 		return user;
 	}
 
-	
-	
-	private Map<String, User> us = new HashMap<String, User>(); 
+	private Map<String, User> us = new HashMap<String, User>();
 }
--- junit-study/src/test/java/com/laolang/junitstudy/service/TestUserService.java
+++ junit-study/src/test/java/com/laolang/junitstudy/service/TestUserService.java
@@ -8,94 +8,84 @@ import com.laolang.junitstudy.model.UserException;
 
 import static org.junit.Assert.*;
 
-
 public class TestUserService {
 
 	private IUserService userservice;
 	private User baseUser;
-	
-	private void assertUserEquals( User user, User testuser){
-		assertEquals(user.getUsername(),testuser.getUsername());
-		assertEquals(user.getNickname(),testuser.getNickname());
-		assertEquals(user.getPassword(),testuser.getPassword());
+
+	private void assertUserEquals(User user, User testuser) {
+		assertEquals(user.getUsername(), testuser.getUsername());
+		assertEquals(user.getNickname(), testuser.getNickname());
+		assertEquals(user.getPassword(), testuser.getPassword());
 	}
-	
+
 	@Before
-	public void setUp(){
-		//初始化
+	public void setUp() {
+		// 初始化
 		userservice = new UserServiceByMap();
 		baseUser = new User("老狼", "laolang", "123");
 	}
-	
+
 	@Test
-	public void testAdd(){
+	public void testAdd() {
 		userservice.add(baseUser);
-		
+
 		User testuser = userservice.load("laolang");
 		assertNotNull(testuser);
-		
+
 		assertUserEquals(testuser, baseUser);
-//		fail("加入--  testAdd  -- 代码");
+		// fail("加入-- testAdd -- 代码");
 	}
-	
-	@Test(expected=UserException.class)
-	public void testAddExistUserName(){
+
+	@Test(expected = UserException.class)
+	public void testAddExistUserName() {
 		userservice.add(baseUser);
 		User testuser = new User("老狼", "laolang", "123");
 		userservice.add(testuser);
-		
-//		fail("加入--  testAddExistUserName  -- 代码");
+
+		// fail("加入-- testAddExistUserName -- 代码");
 	}
-	
-	
+
 	@Test
-	public void testDelete(){
+	public void testDelete() {
 		userservice.add(baseUser);
 		User testuser = userservice.load(baseUser.getUsername());
 		assertNotNull(testuser);
 		userservice.delete(baseUser.getUsername());
 		testuser = userservice.load(baseUser.getUsername());
-		assertNull(testuser);
-		
-//		fail("加入--  testDelete  -- 代码");
+		assertNull("删除的用户还存在", testuser);
+
+		// fail("加入-- testDelete -- 代码");
 	}
-	
-//	@Test
-//	public void testLoad(){
-//		
-//		
-//		fail("加入--  testLoad  -- 代码");
-//	}
-	
+
 	@Test
-	public void testLogin(){
-		
+	public void testLogin() {
+
 		userservice.add(baseUser);
 		String username = "laolang";
 		String password = "123";
-		
+
 		User testuser = userservice.login(username, password);
 		assertUserEquals(baseUser, testuser);
-		
-//		fail("加入--  testLogin  -- 代码");
+
+		// fail("加入-- testLogin -- 代码");
 	}
-	
-	
-	@Test(expected=UserException.class)
-	public void testNotExistUserLogin(){
+
+	@Test(expected = UserException.class)
+	public void testNotExistUserLogin() {
 		userservice.add(baseUser);
 		String username = "laolang0";
 		String password = "123";
 		userservice.login(username, password);
-//		fail("加入--  testNotExistUserLogin  -- 代码");
+		// fail("加入-- testNotExistUserLogin -- 代码");
 	}
-	
-	@Test(expected=UserException.class)
-	public void testPasswordErrorUserLogin(){
+
+	@Test(expected = UserException.class)
+	public void testPasswordErrorUserLogin() {
 		userservice.add(baseUser);
 		String username = "laolang";
 		String password = "1230";
 		userservice.login(username, password);
-//		fail("加入--  testPasswordErrorUserLogin  -- 代码");
+		// fail("加入-- testPasswordErrorUserLogin -- 代码");
 	}
 }
